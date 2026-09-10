b0b4bf3d40e34f6780d27bd0eb6fc6259df7cd32
Manish Jain <examreg.manish@gmail.com>
2026-09-11 04:36:35 +0530

    v3.3911114 shows fix + webp fix

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,12 @@ const nextConfig: NextConfig = {
     {
       protocol: "https",
       hostname: "assets-in.bmscdn.com",
-    },
+    }, {
+        protocol: "https",
+        hostname: "d10y46cwh6y6x1.cloudfront.net",
+        port: "",
+        pathname: "/images/**",
+   }
   ],
 
   deviceSizes: [640, 768, 1024, 1280],
